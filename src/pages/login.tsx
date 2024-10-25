88cecc6416944c33fa0d591fbbd5469420e77b3f
callumbutteryfd <callum.buttery@fanduel.com>
2024-10-25 14:48:01 +0100

    Fix tailwind issue

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -2,8 +2,8 @@ import React from "react";
 
 export const Login: React.FC = () => {
 	return (
-		<div>
-			<h1>Login</h1>
+		<div className="min-h-screen bg-gray-100 py-6 flex flex-col justify-center sm:py-12">
+			<h1 className="text-3xl">Login</h1>
 		</div>
 	);
 };
