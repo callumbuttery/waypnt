4f37b17559ded28cb326cbebbe4e2548ca3ec280
callumbutteryfd <callum.buttery@fanduel.com>
2024-10-25 16:15:14 +0100

    Auth google login

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,19 +1,22 @@
+import { GoogleLogin } from "@react-oauth/google";
 import React from "react";
 
 export const Login: React.FC = () => {
+	const handleSuccess = (response: any) => {
+		console.log("Success: ", response);
+	};
+
+	const handleFailure = () => {
+		console.log("Failure");
+	};
+
 	return (
 		<div className="bg-login bg-centre">
 			<div className="h-screen flex justify-center items-center">
-				<div className="bg-white mx-4 p-8 rounded shadow-md h-2/6 w-1/6">
+				<div className="bg-white mx-4 p-8 rounded shadow-md h-2/6 w-1/6 rounded-2xl">
 					<h1 className="text-3xl font-bold mb-8 text-center">Login</h1>
-
-					<div className="my-10 w-full flex flex-row justify-center items-center">
-						<button
-							className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
-							type="button"
-						>
-							Login
-						</button>
+					<div className="mt-28 w-full flex flex-row justify-center items-center">
+						<GoogleLogin onSuccess={handleSuccess} onError={handleFailure} />
 					</div>
 				</div>
 			</div>
